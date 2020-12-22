7cac97bcf142406a9ba650859dcfeb8281436e76
Lam-Pham <lpham127@gmail.com>
2020-12-22 15:38:24 -0800

    lowered title size

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,9 @@
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h3 className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Beginner's Luck for Everyone
-      </h1>
+      </h3>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         the official blog of our fantasy basketball league (lol)
       </h4>
